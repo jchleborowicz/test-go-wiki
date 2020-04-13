1a543911b9eb21daaac5a8e264ca404cbd38a34c
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2020-04-13 20:53:46 +0200

    Initial commit.

---
+++ wiki.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+}
